64d43cdb8c5ed91cde2017ad470f99b3162b5ecc
Julia Stahl <julia.stahl@kepha.com.br>
2023-01-27 13:55:16 -0300

    Importa pacote perfil e exporta app_pages

--- lib/dependency_manager.dart
+++ lib/dependency_manager.dart
@@ -1,7 +1,3 @@
 library dependency_manager;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:perfil/app/routes/app_pages.dart';
